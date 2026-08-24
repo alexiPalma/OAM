16a2c8581c8f5d932fbf44bdf5c15f9e852d9793
alexiPalma <a.palma@internet.ru>
2026-08-24 15:21:05 +0500

    Load complete message and start editors

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import asyncio
 import bot
 import admin_panel_patch
-import message_editor  # full editor for every user/admin message
+import message_editor
+import start_editor
 
 if __name__ == '__main__':
     asyncio.run(bot.main())
