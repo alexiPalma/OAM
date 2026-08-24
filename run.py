e57b1ba2cc5a3bbdc0d5661b3318b631121e82ef
alexiPalma <a.palma@internet.ru>
2026-08-24 14:48:38 +0500

    Wire profile cooldown and kill statistics into launcher

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import asyncio
 import bot
-from battle_runtime import install
+from battle_runtime import install as install_battle
+from profile_patch import install as install_profile
 
-install(bot)
+install_battle(bot)
+install_profile(bot)
 
 if __name__ == '__main__':
     asyncio.run(bot.main())
