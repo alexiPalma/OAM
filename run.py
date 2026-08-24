6e07a7383ddfb3e69de9e7ab442a91173caf2033
alexiPalma <a.palma@internet.ru>
2026-08-24 15:15:36 +0500

    Wire functional admin panel before bot dispatcher starts

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import asyncio
 import bot
+import admin_panel_patch  # replaces the placeholder admin sections before Dispatcher registration
 
 if __name__ == '__main__':
     asyncio.run(bot.main())
