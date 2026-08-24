96f465fae3fa1c1b71b0c1810b232b0c55a856ab
alexiPalma <a.palma@internet.ru>
2026-08-24 23:13:43 +0500

    run: load final WorldWarDynasty hotfix layer

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 import asyncio
 import bot
 import runtime_fix
+import hotfix_2026_08_24
 
 runtime_fix.install()
+hotfix_2026_08_24.install()
 
 if __name__ == '__main__':
     asyncio.run(bot.main())
