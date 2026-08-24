f6c4fa5db64fb78c6891d0d3ac527e45bdf05f53
alexiPalma <a.palma@internet.ru>
2026-08-24 23:14:03 +0500

    run: apply final keyword and promo hotfix

--- run.py
+++ run.py
@@ -2,9 +2,11 @@ import asyncio
 import bot
 import runtime_fix
 import hotfix_2026_08_24
+import hotfix_final
 
 runtime_fix.install()
 hotfix_2026_08_24.install()
+hotfix_final.install()
 
 if __name__ == '__main__':
     asyncio.run(bot.main())
